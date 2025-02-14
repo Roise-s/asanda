3a51749d9000b7b5fe535c387a267d81fe869049
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-02-14 11:11:55 +0200

    Done with Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import './main.css'
 import pic from '../../assets/asanda.png'
+import { BiLogoTelegram } from "react-icons/bi";
+import { FaTiktok } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
 
 const Main = () => {
   return (
     <div className='main'>
       <div className="one">
-        social
+        <p className="join">
+          Join the community
+        </p>
+
+        <div className="btnsdiv">
+          <button className="linkBtns">
+            <BiLogoTelegram className='Bilogo' /> Telegram
+          </button>
+          <button className="linkBtns">
+            <FaTiktok className='Bilogo' /> TikTok
+          </button>
+          <button className="linkBtns">
+            <IoLogoYoutube className='Bilogo' /> YouTube
+          </button>
+          <button className="linkBtns">
+          <FaInstagram className='Bilogo' /> Instagram
+          </button>
+        </div>
       </div>
       <div className="oneoand">
         <img src={pic} alt="pic" className='picone' />
